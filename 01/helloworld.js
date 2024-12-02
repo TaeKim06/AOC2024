0818ff3494066f534be1b5e0390e65708a542617
Tae Kim <taekim1942@gmail.com>
2024-12-02 18:45:30 -0500

    initial commit: Setting up file structure

---
+++ 01/helloworld.js
@@ -0,0 +1,12 @@
+const greet = (name) => {
+    console.log(`Hello, ${name}!`);
+};
+
+const sum = (a, b) => {
+    return a + b;
+};
+const my_name = `Tae Kim`;
+greet(my_name);
+
+const result = sum(4, 5);
+console.log(`The sum of 4 and 5 is: ${result}`)             
